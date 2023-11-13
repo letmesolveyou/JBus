b3e4f30acb62644d0938aa1d07a7323dc93abc3a
letmesolveyou <safia.amita@gmail.com>
2023-11-13 22:57:28 +0700

    PT07 - Menyelesaikan Post Test Modul 7 Revisi

--- src/main/java/com.SafiaAmitaJBusBR/Account.java
+++ src/main/java/com.SafiaAmitaJBusBR/Account.java
@@ -41,6 +41,7 @@ public class Account extends Serializable {
 
     public boolean topUp(double amount) {
         if (amount > 0) {
+            this.balance += amount;
             return true;
         }
         return false;
